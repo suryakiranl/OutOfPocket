a809aefa00260fc9ecaea5a09597f4e0dc96eb83
Surya Kiran <suryakiranl@gmail.com>
2014-02-12 21:55:25 -0800

    DELETE api for user

--- src/main/java/com/surya/oop/service/UserService.java
+++ src/main/java/com/surya/oop/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -72,6 +73,11 @@ public class UserService {
 		System.out.println("Inside POST method of User :" + user);
 		
 		return user;
+	}
+	
+	@DELETE
+	public void deleteUser(User user) {
+		
 	}
 	
 }
